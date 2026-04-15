6a8ec2c867fe87a3ab71ee66067dc075efa6233b
njogubless <pnjogubless@gmail.com>
2026-04-15 10:28:37 +0300

    feature: orders data- models

--- marketplacebackend/orders/models.py
+++ marketplacebackend/orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Order(models.Model):
+    buyer= models.ForeignKey(User, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    status = models.CharField(max_length=50, default='pending')
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.product.name} x {self.quantity}"
