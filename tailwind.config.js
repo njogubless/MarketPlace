a709f12d46a674820b28f5e711ff9c088880c6a3
njogubless <pnjogubless@gmail.com>
2026-04-16 16:34:08 +0300

    chore: configure tailwind css , post css and update entry point jsx

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neon-purple': '#a855f7',
+        'void': {
+          700: '#1f2937',
+          800: '#111827',
+          900: '#0a0a0f',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
