b9305f8f9cd23a67c19c318b3db6a77aceb21da9
njogubless <pnjogubless@gmail.com>
2026-04-17 16:14:52 +0300

    fix:(accounts): move create() out of Meta clas and add UserSerializer

--- marketplacebackend/accounts/serializers.py
+++ marketplacebackend/accounts/serializers.py
@@ -2,14 +2,23 @@ from rest_framework import serializers
 from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=6)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'is_vendor']
 
-        def create(self, validated_data):
-            user=user.objects.create_user(
-                **validated_data
-            )
-            return user
+    def create(self, validated_data):
+        user=user.objects.create_user(
+         username=validated_data['username'],
+         email=validated_data.get('email', ''),
+         password= validated_data['password'],
+         is_vendor=validated_data.get('is_vendor', False),
+      )
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields =['id','username', 'email', 'is_vendor', 'bio', 'avatar']
