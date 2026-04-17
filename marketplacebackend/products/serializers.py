ac9ec1199bf6ae934de24676a6e8754a10dff333
njogubless <pnjogubless@gmail.com>
2026-04-17 16:00:51 +0300

    feat:(product): add categorySerializer and enrich ProductSerializer with compund fields

--- marketplacebackend/products/serializers.py
+++ marketplacebackend/products/serializers.py
@@ -2,6 +2,16 @@ from rest_framework import serializers
 from.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    in_stock = serializers.BooleanField(read_only=True)
+    avg_rating = serializers.FloatField(read_only=True)
+    review_count = serializers.SerializerMethodField()
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'price', 'price_old', 'image',
+                  'stock', 'in_stock', 'is_featured', 'badge', 'category', 
+                  'category_name', 'avg_rating', 'review_count','created_at']
+        
+    def get_review_count(self, obj):
+        return obj.reviews.count()
