ddb77faaa0ea2a0871e79ed5e23d8e87b021c440
njogubless <pnjogubless@gmail.com>
2026-04-18 10:08:48 +0300

    feat:(vendors): the interface of the api interface

--- marketplacebackend/vendors/views.py
+++ marketplacebackend/vendors/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Vendor
+from .serializers import VendorSerializer
 
-# Create your views here.
+class VendorViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Vendor.objects.all().select_related('stats')
+    serializer_class = VendorSerializer
+    permission_classes = [permission.AllowAny]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        search = self.request.query_params.get('search')
+        if search:
+            qs = qs.filter(shop_name__icontains=search)
+        return qs
