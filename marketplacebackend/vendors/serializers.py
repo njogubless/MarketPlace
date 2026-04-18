b9e546822d7f3249cb1c09e4b7accdae61b91fad
njogubless <pnjogubless@gmail.com>
2026-04-18 10:07:50 +0300

    feat:(vendors): serializng the models

---
+++ marketplacebackend/vendors/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Vendor, VendorStats
+
+class VendorStatsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VendorStats
+        fields= '__all__'
+
+
+class VendorSerializer(serializers.ModelSerializer):
+    stats= VendorStatsSerializer(read_only=True)
+
+    class Meta:
+        models = Vendor
+        fields = ['id', 'shop_name', 'handle', 'description', 'logo', 'location', 'is_verified', 'created_at', 'stats']
+        
