3c33c96be0719b653136347fc9e702ae71d22203
njogubless <pnjogubless@gmail.com>
2026-04-18 12:21:29 +0300

    fix(product): adding arguments for the seed data

--- marketplacebackend/products/models.py
+++ marketplacebackend/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 User= settings.AUTH_USER_MODEL
 
@@ -8,7 +9,10 @@ class Category(models.Model):
     name=models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True)
 
-    def __Str__(self):
+    description = models.TextField(blank=True)
+    icon = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
         return self.name
     
     class Meta:
@@ -18,6 +22,7 @@ class Product(models.Model):
     seller = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='products')
     name= models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     price_old = models.DecimalField(max_digits=10,decimal_places=2, null=True, blank=True)
@@ -27,7 +32,12 @@ class Product(models.Model):
     badge = models.CharField(max_length=50, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
+    specs = models.JSONField(blank=True, null=True)
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -37,7 +47,7 @@ class Product(models.Model):
         return self.stock > 0
     
     @property
-    def avg_rating(Self):
+    def avg_rating(self):
         reviews = self.reviews.all()
         if not reviews:
             return 0
