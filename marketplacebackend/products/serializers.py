f83c71f53a9326c90e46075c4037b2c0c1a3ef9a
njogubless <pnjogubless@gmail.com>
2026-04-18 10:18:42 +0300

    fix:(products): adding caegorySerializer

--- marketplacebackend/products/serializers.py
+++ marketplacebackend/products/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
-from.models import Product
+from.models import Category, Product
 
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields= ['id', 'name', 'slug']
 class ProductSerializer(serializers.ModelSerializer):
 
     category_name = serializers.CharField(source='category.name', read_only=True)
