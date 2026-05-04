76618611282914ce200146bb1a8cf110637cea2f
njogubless <pnjogubless@gmail.com>
2026-05-04 19:24:49 +0300

    fix(accounts):set USERNAME_FIELD to email and unique=TRUE for email login

--- marketplacebackend/accounts/models.py
+++ marketplacebackend/accounts/models.py
@@ -2,9 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     is_vendor = models.BooleanField(default=False)
     bio = models.TextField(blank=True)
     avatar = models.URLField(blank=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
     def __str__(self):
-        return self.username
+        return self.email
