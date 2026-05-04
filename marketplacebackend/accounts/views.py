08b1115b8ba84e73e4d74c6f806fe65f39e334c1
njogubless <pnjogubless@gmail.com>
2026-05-04 19:26:47 +0300

    fix(accounts): add RegisterView, MeView and EmailTokenObtainPairView for email based JWT login

--- marketplacebackend/accounts/views.py
+++ marketplacebackend/accounts/views.py
@@ -4,14 +4,32 @@ from rest_framework.views import APIView
 from .models import User
 from .serializers import RegisterSerializer, UserSerializer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+
+#User = get_user_model()
+
 class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
 
 
-class MeView(APIView):
+class MeView(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
 
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+
+
+class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
+    username_field = 'email'
+
+
+class EmailTokenObtainPairView(TokenObtainPairView):
+    serializer_class = EmailTokenObtainPairSerializer
