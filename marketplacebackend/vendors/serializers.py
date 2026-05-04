fc5dbd5ddbf1e0804ddd52b2f21837389ea57786
njogubless <pnjogubless@gmail.com>
2026-05-04 19:31:27 +0300

    fix(marketplacebackend): placing proper fields for the serializer class

--- marketplacebackend/vendors/serializers.py
+++ marketplacebackend/vendors/serializers.py
@@ -4,12 +4,12 @@ from .models import Vendor, VendorStats
 class VendorStatsSerializer(serializers.ModelSerializer):
     class Meta:
         model = VendorStats
-        fields= '__all__'
-
+        fields = ['total_sales', 'total_revenue', 'avg_rating', 'total_reviews']
 
 class VendorSerializer(serializers.ModelSerializer):
-    stats= VendorStatsSerializer(read_only=True)
+    stats = VendorStatsSerializer(read_only=True)  
 
     class Meta:
         model = Vendor
-        fields = ['id', 'shop_name', 'handle', 'description', 'logo', 'location', 'is_verified', 'created_at', 'stats']
+        fields = ['id', 'shop_name', 'handle', 'description',
+                  'logo', 'location', 'is_verified', 'created_at', 'stats']
